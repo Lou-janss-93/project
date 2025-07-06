cf1421d20aabd336f675bcd0f5d0d407f8a46a95
Louis_Janss. <janssens_lou88@hotmail.com>
2025-07-06 18:27:19 +0200

    Fix onboarding personality names

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -60,7 +60,7 @@ export interface Translations {
   // Onboarding
   onboarding: {
     choosePersona: string;
-    chooseDescription: string;
+    chooseDescription: 'Selecteer hoe je jezelf wilt presenteren: Roots, Mask, of Spark.',
     rootsTitle: string;
     rootsDesc: string;
     maskTitle: string;
